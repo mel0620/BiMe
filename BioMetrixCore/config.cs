ee08bbab9f9469ae9fd350f2f3caaf548265b0a0
kimbriel <kimbriel21@gmail.com>
2017-09-25 14:42:17 +0800

    sabi ni rommel commit ko na

---
+++ BioMetrixCore/config.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BioMetrixCore
+{
+    class config
+    {
+        public static String appkey = "dwMlq"; // Modifiable
+        public static String appsecret = "454afdb9a7f257be00949fde46be6fce"; // Modifiable
+        public static String branchid = "1";
+        public static String email = "";
+        public static String password = "";
+    }
+}
